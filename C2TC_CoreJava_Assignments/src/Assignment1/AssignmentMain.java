60f0fd0abd977f0d57227ec890e6c4b6527fb609
Baasith04 <baasithsulaiman2004@gmail.com>
2025-08-19 15:24:58 +0530

    Commit

--- C2TC_CoreJava_Assignments/src/Assignment1/AssignmentMain.java
+++ C2TC_CoreJava_Assignments/src/Assignment1/AssignmentMain.java
@@ -11,53 +11,20 @@ public class AssignmentMain {
         System.out.println("--- Java Packages and Access Modifiers ---\n");
         
         Manager manager1 = new Manager("Mohamed Baasith", 1001, 95000.0, "Engineering", 
-                                     8, "Senior", "Annual budget of $2M");
+                                     8, "Senior");
 
         Manager manager2 = new Manager("Akshaya", 1002, 88000.0, "Marketing", 
-                                     5, "Mid", "Quarterly budget of $500K");
+                                     5, "Mid");
 
         Developer developer1 = new Developer("Devakumaran", 2001, 75000.0, "Engineering", 
-                                           "Java", 5, "E-commerce Platform");
+                                           "Java", 5);
 
         Developer developer2 = new Developer("Nivetha", 2002, 72000.0, "Engineering", 
-                                           "Python", 3, "Data Analytics System");
+                                           "Python", 3);
 
         Developer developer3 = new Developer("Kokila", 2003, 80000.0, "Engineering", 
-                                           "JavaScript", 7, "Frontend Redesign");
+                                           "JavaScript", 7);
 
-        
-        
-        System.out.println("Adding employees to the system...");
-        EmployeeUtilities.addEmployee(manager1);
-        EmployeeUtilities.addEmployee(manager2);
-        EmployeeUtilities.addEmployee(developer1);
-        EmployeeUtilities.addEmployee(developer2);
-        EmployeeUtilities.addEmployee(developer3);
-        System.out.println("All employees added successfully!\n");
-        
-        EmployeeUtilities.displayAllEmployees();
-        System.out.println();
-        System.out.println("Manager 1 Name: " + manager1.getName()); 
-        System.out.println("Developer 1 Programming Language: " + developer1.getProgrammingLanguage());
-        System.out.println();
-        
-        System.out.printf("Total Salary Expense: $%.2f%n", 
-                         EmployeeUtilities.calculateTotalSalaryExpense());
-        
-        System.out.printf("Average Engineering Department Salary: $%.2f%n", 
-                         EmployeeUtilities.calculateAverageSalaryByDepartment("Engineering"));
-        
-        System.out.printf("Average Marketing Department Salary: $%.2f%n", 
-                         EmployeeUtilities.calculateAverageSalaryByDepartment("Marketing"));
-        System.out.println();
-   
-        Employee foundEmployee = EmployeeUtilities.findEmployeeById(2001);
-        if (foundEmployee != null) {
-            System.out.println("Found: " + foundEmployee.toString());
-        }
-        System.out.println();
-        
-        
         System.out.println("--- All Managers ---");
         Manager[] managers = EmployeeUtilities.getAllManagers();
         for (int i = 0; i < managers.length; i++) {
@@ -71,8 +38,6 @@ public class AssignmentMain {
             System.out.println(developers[i].toString());
         }
         System.out.println();
-        
-        
-        System.out.println("\n--- Program Completed Successfully ---");
     }
 }
+
