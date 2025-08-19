681f6fd0e1494b5526830b6688522c9c01f9bb41
Baasith04 <baasithsulaiman2004@gmail.com>
2025-08-19 14:34:03 +0530

    Commit

--- C2TC_CoreJava_Assignments/src/Assignment1/employees/Manager.java
+++ C2TC_CoreJava_Assignments/src/Assignment1/employees/Manager.java
@@ -4,14 +4,12 @@ public class Manager extends Employee {
     
     private int teamSize;
     private String managementLevel;
-    String budgetResponsibility;
  
     public Manager(String name, int employeeId, double salary, String department, 
-                   int teamSize, String managementLevel, String budgetResponsibility) {
+                   int teamSize, String managementLevel) {
     	super(name, employeeId, salary, department);
     	this.teamSize = teamSize;
         this.managementLevel = managementLevel;
-        this.budgetResponsibility = budgetResponsibility;
     }
     
     public int getTeamSize() {
@@ -29,27 +27,5 @@ public class Manager extends Employee {
     public void setManagementLevel(String managementLevel) {
         this.managementLevel = managementLevel;
     }
-    
-    public String getBudgetResponsibility() {
-        return budgetResponsibility;
-    }
-    
-    public void setBudgetResponsibility(String budgetResponsibility) {
-        this.budgetResponsibility = budgetResponsibility;
-    }
-    
-    @Override
-    protected double calculateAnnualBonus() {
-        double baseBonus = super.calculateAnnualBonus();
-        double teamBonus = teamSize * 1000; // $1000 per team member
-        return baseBonus + teamBonus;
-    }
-    
-    @Override
-    public String toString() {
-        return String.format("Manager[ID: %d, Name: %s, Department: %s, Salary: $%.2f, " +
-                           "Team Size: %d, Management Level: %s]", 
-                           getEmployeeId(), getName(), getDepartment(), getSalary(),
-                           teamSize, managementLevel);
     }
 }
