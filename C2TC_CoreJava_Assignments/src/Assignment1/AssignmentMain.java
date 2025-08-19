9fde464ac722df745f9cdaf9e0772630902cd50c
Baasith04 <baasithsulaiman2004@gmail.com>
2025-08-19 15:26:28 +0530

    Commit

--- C2TC_CoreJava_Assignments/src/Assignment1/AssignmentMain.java
+++ C2TC_CoreJava_Assignments/src/Assignment1/AssignmentMain.java
@@ -10,34 +10,35 @@ public class AssignmentMain {
     public static void main(String[] args) {
         System.out.println("--- Java Packages and Access Modifiers ---\n");
         
-        Manager manager1 = new Manager("Mohamed Baasith", 1001, 95000.0, "Engineering", 
+        Manager m1 = new Manager("Mohamed Baasith", 1001, 95000.0, "Engineering", 
                                      8, "Senior");
 
-        Manager manager2 = new Manager("Akshaya", 1002, 88000.0, "Marketing", 
+        Manager m2 = new Manager("Akshaya", 1002, 88000.0, "Marketing", 
                                      5, "Mid");
 
-        Developer developer1 = new Developer("Devakumaran", 2001, 75000.0, "Engineering", 
+        Developer d1 = new Developer("Devakumaran", 2001, 75000.0, "Engineering", 
                                            "Java", 5);
 
-        Developer developer2 = new Developer("Nivetha", 2002, 72000.0, "Engineering", 
+        Developer d2 = new Developer("Nivetha", 2002, 72000.0, "Engineering", 
                                            "Python", 3);
 
-        Developer developer3 = new Developer("Kokila", 2003, 80000.0, "Engineering", 
+        Developer d3 = new Developer("Kokila", 2003, 80000.0, "Engineering", 
                                            "JavaScript", 7);
 
         System.out.println("--- All Managers ---");
-        Manager[] managers = EmployeeUtilities.getAllManagers();
-        for (int i = 0; i < managers.length; i++) {
-            System.out.println(managers[i].toString());
+        Manager[] mn = EmployeeUtilities.getAllManagers();
+        for (int i = 0; i < mn.length; i++) {
+            System.out.println(mn[i].toString());
         }
         System.out.println();
         
         System.out.println("--- All Developers ---");
-        Developer[] developers = EmployeeUtilities.getAllDevelopers();
-        for (int i = 0; i < developers.length; i++) {
-            System.out.println(developers[i].toString());
+        Developer[] ds = EmployeeUtilities.getAllDevelopers();
+        for (int i = 0; i < ds.length; i++) {
+            System.out.println(ds[i].toString());
         }
         System.out.println();
     }
 }
+
 
