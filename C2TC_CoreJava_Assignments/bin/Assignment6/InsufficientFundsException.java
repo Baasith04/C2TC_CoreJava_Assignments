f6132d278ee5f20ca0cac02bb4f786bf020cb917
Baasith04 <baasithsulaiman2004@gmail.com>
2025-08-29 09:34:05 +0530

    Commit

---
+++ C2TC_CoreJava_Assignments/bin/Assignment6/InsufficientFundsException.java
@@ -0,0 +1,8 @@
+package Assignment6;
+
+//InsufficientFundsException.java
+public class InsufficientFundsException extends Exception {
+public InsufficientFundsException(String message) {
+   super(message);
+}
+}
